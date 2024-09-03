4a202b204e17b4c74165fd7b708690b958b3d478
Corey B <corey@Coreys-MacBook-Pro.local>
2024-09-03 18:09:57 -0400

    Initial commit

---
+++ src/main/java/com/quaternary_group3/QuaternaryCalculator/QuaternaryCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.quaternary_group3.QuaternaryCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuaternaryCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuaternaryCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quaternary_group3/QuaternaryCalculator/QuaternaryCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quaternary_group3.QuaternaryCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuaternaryCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
